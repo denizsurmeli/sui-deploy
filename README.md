928cdd7ced7f0db2dd8dcc0668366162ced37d66
Deniz Surmeli <denizsurmeli@icloud.com>
2022-08-03 17:24:18 +0300

    push script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sui-deploy
 Deploying script for Sui Testnet
+Run `fullnode.sh <github_username>` on a EC2 instance with Ubuntu. 
+Make sure that you have forked the chain with your Github account.
+Deploys by fullnode with default configurations.
+
+## Usage
+```
+fullnode.sh github_username
+```
---
+++ fullnode.sh
@@ -0,0 +1,21 @@
+apt-get update \
+    && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends \
+    tzdata \
+    git \
+    ca-certificates \
+    curl \
+    build-essential \
+    libssl-dev \
+    pkg-config \
+    libclang-dev \
+    cmake
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+git clone https://github.com/$1/sui.git
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout --track upstream/devnet
+cp crates/sui-config/data/fullnode-template.yaml fullnode.yaml
+curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+cargo run --release --bin sui-node -- --config-path fullnode.yaml
