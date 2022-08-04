633d8b93385c38594b6de1cbc794ace1e88a8ba0
Deniz Surmeli <denizsurmeli@icloud.com>
2022-08-04 08:29:02 +0300

    update script

---
+++ update.sh
@@ -0,0 +1,14 @@
+systemctl stop node
+rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
+source $HOME/.cargo/env
+cd $HOME
+git clone https://github.com/MystenLabs/sui.git
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout -B devnet --track upstream/devnet
+cargo build --release
+mv ~/sui/target/release/sui-node /usr/local/bin/
+mv ~/sui/target/release/sui /usr/local/bin/
+wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+systemctl restart node
